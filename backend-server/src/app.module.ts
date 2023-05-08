030430f8af47f8ef7f585675e1ac702e2c73d840
yjin-01 <t781797@gmail.com>
2023-05-08 13:57:58 +0900

    :tada: setting nestJS & graphql
    nestjs boilerplate

---
+++ backend-server/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/commons/grahql/schema.gql'),
+      context: ({ req, res }) => ({ req, res }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
