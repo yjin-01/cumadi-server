f8f20979305a4698f66875609e9219a15256f654
yjin-01 <t781797@gmail.com>
2023-05-29 20:54:53 +0900

    :ambulance: Chore: exception-filter
    Chore: exception-filter

--- backend-server/src/main.ts
+++ backend-server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { graphqlUploadExpress } from 'graphql-upload';
-import { CustomExceptionFilter } from './commons/filter/http-exception.filter';
+import { CustomExceptionFilter } from './commons/filter/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
