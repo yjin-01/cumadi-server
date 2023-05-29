855488f70c8eec170760b8e59fbe4277b0802a7f
yjin-01 <t781797@gmail.com>
2023-05-29 20:53:18 +0900

    :ambulance: Chore: exception-filter
    Chore: exception-filter

--- backend-server/src/commons/filter/exception.filter.ts
+++ backend-server/src/commons/filter/exception.filter.ts
@@ -3,6 +3,7 @@ import {
   HttpException,
   ExceptionFilter,
   HttpStatus,
+  NotFoundException,
 } from '@nestjs/common';
 import { AxiosError } from 'axios';
 import { GraphQLError } from 'graphql';
@@ -10,18 +11,28 @@ import { GraphQLError } from 'graphql';
 @Catch()
 export class CustomExceptionFilter implements ExceptionFilter {
   catch(exception: unknown) {
-    console.log(exception);
     const error = {
       status: HttpStatus.INTERNAL_SERVER_ERROR,
       message: '예외 발생',
     };
 
-    if (exception instanceof HttpException) {
+    if (exception instanceof NotFoundException) {
+      error.status = 404;
+      error.message = '잘못된 페이지 요청';
+    } else if (exception instanceof HttpException) {
       error.status = exception.getStatus();
       error.message = exception.message;
+
+      throw new GraphQLError(error.message, {
+        extensions: { code: error.status },
+      });
     } else if (exception instanceof AxiosError) {
       error.status = exception.response.status;
       error.message = exception.response.data.message;
+
+      throw new GraphQLError(error.message, {
+        extensions: { code: error.status },
+      });
     }
 
     console.log('============');
@@ -29,9 +40,5 @@ export class CustomExceptionFilter implements ExceptionFilter {
     console.log('예외내용: ', error.message);
     console.log('예외코드: ', error.status);
     console.log('============');
-
-    // throw new GraphQLError(error.message, {
-    //   extensions: { code: error.status },
-    // });
   }
 }
